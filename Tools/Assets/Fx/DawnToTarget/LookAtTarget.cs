42288e72cc305924ecc0d5b151aabb3dcc56bab5
Shachar Oz <shachar.oz@gmail.com>
2019-10-03 22:38:59 +0200

    art phone, fxs, code

---
+++ Tools/Assets/Fx/DawnToTarget/LookAtTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+    private Transform realTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        realTarget = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p = Target.position + Offset;
+        realTarget.position = p;
+        transform.LookAt(realTarget);
+    }
+}
