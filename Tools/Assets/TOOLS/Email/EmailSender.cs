a35fb8244038f233456c233ec7bde71e4771a8cb
Shachar Oz <shachar.oz@gmail.com>
2020-01-14 00:16:58 +0100

    emailer

--- Tools/Assets/TOOLS/Email/EmailSender.cs
+++ Tools/Assets/TOOLS/Email/EmailSender.cs
@@ -6,6 +6,13 @@ using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
 
+
+//
+//  last method Emailer.cs
+//  http://www.mrventures.net/all-tutorials/sending-emails
+using System.Collections;
+
+
 public class EmailSender : MonoBehaviour
 {
     //send email by opening a email software before that
@@ -32,19 +39,124 @@ public class EmailSender : MonoBehaviour
     //send immediately from code without nothing
     public void SendEmailImmediately() {
         MailMessage mail = new MailMessage();
-        mail.From = new MailAddress("shachar.oz@gmail.com");
-        mail.To.Add("we@flux-experiences.com");
+        mail.From = new MailAddress(kSenderEmailAddress);
+        mail.To.Add(kReceiverEmailAddress);
         mail.Subject = "SMTP New Order: warehouse " + System.DateTime.Now.Date;
         mail.Body = "SMTP My Body\r\nFull of non-escaped chars";
         // you can use others too.
         SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
         smtpServer.Port = 587;
-        smtpServer.Credentials = new NetworkCredential("shachar.oz@gmail.com", "yourpass") as ICredentialsByHost;
+        smtpServer.Credentials = new NetworkCredential("shachar.oz@gmail.com", "theworldatyourfeet1") as ICredentialsByHost;
         smtpServer.EnableSsl = true;
         ServicePointManager.ServerCertificateValidationCallback =
         delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         { return true; };
 
-        smtpServer.Send(mail);
+        try
+        {
+            smtpServer.Send(mail);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("Email error: " + e.Message);
+        }
+        finally
+        {
+            Debug.Log("Email sent!");
+        }
+    }
+
+
+
+
+
+
+
+    //from new guy
+    const string kSenderEmailAddress = "CoderBoy6000@gmail.com";
+    const string kSenderPassword = "CoderBoy6000!!!";
+    const string kReceiverEmailAddress = "we@flux-experiences.com";
+
+    // Method 2: Server request
+    const string url = "https://coderboy6000.000webhostapp.com/emailer.php";
+
+
+    public UnityEngine.UI.Button btnSubmit;
+
+    private void Start()
+    {
+        btnSubmit.onClick.AddListener(delegate {
+             SendAnEmail("asdasdsd");
+        });
+    }
+    // Method 1: Direct message 
+    public static void SendAnEmail(string message)
+    {
+        // Create mail
+        MailMessage mail = new MailMessage();
+        mail.From = new MailAddress(kSenderEmailAddress);
+        mail.To.Add(kReceiverEmailAddress);
+        mail.Subject = "Email Title";
+        mail.Body = message;
+
+        // Setup server 
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new NetworkCredential(
+            kSenderEmailAddress, kSenderPassword) as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate,
+            X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+                Debug.Log("Email success!");
+                return true;
+            };
+
+        // Send mail to server, print results
+        try
+        {
+            smtpServer.Send(mail);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("Email error: " + e.Message);
+        }
+        finally
+        {
+            Debug.Log("Email sent!");
+        }
+    }
+
+    // Method 2: Server request
+    public void SendServerRequestForEmail(string message)
+    {
+        StartCoroutine(SendMailRequestToServer(message));
+    }
+
+    // Method 2: Server request
+    static IEnumerator SendMailRequestToServer(string message)
+    {
+        // Setup form responses
+        WWWForm form = new WWWForm();
+        form.AddField("name", "It's me!");
+        form.AddField("fromEmail", kSenderEmailAddress);
+        form.AddField("toEmail", kReceiverEmailAddress);
+        form.AddField("message", message);
+
+        // Submit form to our server, then wait
+        WWW www = new WWW(url, form);
+        Debug.Log("Email sent!");
+
+        yield return www;
+
+        // Print results
+        if (www.error == null)
+        {
+            Debug.Log("WWW Success!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
     }
 }
