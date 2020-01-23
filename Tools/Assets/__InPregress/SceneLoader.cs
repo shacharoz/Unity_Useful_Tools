491a8a7395f6f417e9ebfa49600183659ab62fac
Shachar Oz <shachar.oz@gmail.com>
2020-01-23 09:27:31 +0100

    arranged files

---
+++ Tools/Assets/__InPregress/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void LoadSceneNow(string _sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_sceneName);
+    }
+}
