f23025c9090aae5b524765bd17caaaa0bc47a4bf
Shachar Oz <shachar.oz@prophecy.digital>
2021-05-09 18:53:28 +0200

    platform detection

---
+++ Tools/Assets/TOOLS/PlatformDetector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlatformDetector : MonoBehaviour
+{
+    public UnityEvent OnPlatformAndroidDetected;
+    public UnityEvent OnPlatformIosDetected;
+    public UnityEvent OnPlatformPcDetected;
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            OnPlatformIosDetected.Invoke();
+        }
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            OnPlatformAndroidDetected.Invoke();
+        }
+    }
+
+
+}
